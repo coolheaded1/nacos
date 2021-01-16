ec9b4d7161fb7efdc7bf28e209daa4c78f5364e6
yakubu234 <yakubuabiola2003@gmail.com>
2021-01-16 21:19:05 +0100

    16/01/2021 adding of logout

--- accounts/logout.php
+++ accounts/logout.php
@@ -5,4 +5,6 @@ session_destroy();
 session_write_close();
 setcookie(session_name(),'',0,'/');
 session_regenerate_id(true);
+$msg = "You are login Out";
+header('Location:'.$urlServer.'register.php');
 ?>
